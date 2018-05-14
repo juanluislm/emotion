8e4c82954bd0f556cd64a593384e0a2b5e5e5ab3
azhongwl <azhong@AF051506.employee-wifi.rwc.corp.imvu.com>
2018-05-14 16:25:19 -0700

    use mouth dnn

--- tensorflow/demo_emotion_classifier.py
+++ tensorflow/demo_emotion_classifier.py
@@ -15,11 +15,6 @@ def apply_offsets(face_coordinates, offsets):
     x_off, y_off = offsets
     return (x - x_off, x + width + x_off, y - y_off, y + height + y_off)
 
-with open('mouth_weights_absolute.json') as fi:
-    mouth_weights = json.load(fi)
-    mouth_coef_absolute = mouth_weights['coef']
-    mouth_intercept_absolute = mouth_weights['intercept']
-
 with open('mouth_weights_robust_0.1.json') as fi:
     mouth_weights = json.load(fi)
     mouth_coef_top_down = mouth_weights['top_down_coef']
@@ -43,6 +38,11 @@ landmark_inference = TFInference(model_path = 'models/mtcnn_onet.h5.pb',
                                  output_names = ['import/output_node0',
                                                  'import/output_node1',
                                                  'import/output_node2'])
+# mouth openness model
+mouth_openness_inference = TFInference(model_path = 'models/faceoff_kao_onet_32_lm_12.66-0.31-0.13.hdf5.pb',
+                                       input_name = 'import/input_1',
+                                       output_names = ['import/output_node0'])
+mouth_target_size = mouth_openness_inference.input_shape[0:2]
 
 # starting video streaming
 cv2.namedWindow('window_frame')
@@ -96,37 +96,66 @@ while True:
         diff = (y2-y1)-(x2-x1)
         x1 = max(int(x1-diff/2), 0)
         x2 = min(int(x2+diff/2), width-1)
+    landmark_pts_raw = []
     for i in range(0, 5):
-        cv2.circle(rgb_image, (int(landmark_pts[i]*w+x), int(landmark_pts[i+5]*h+y)), 2, (0, 255, 0))
+        landmark_pts_raw.append((landmark_pts[i]*w+x, landmark_pts[i+5]*h+y))
+        cv2.circle(rgb_image, (int(landmark_pts_raw[i][0]), int(landmark_pts_raw[i][1])), 2, (0, 255, 0))
 
     # --------------------------------------------------
     # step 3: inference of emotion nets for emotion and mouth openness
     # --------------------------------------------------
     face_coordinates = [x1, y1, x2-x1, y2-y1]
     gray_face = gray_image[y1:y2, x1:x2]
+    mouth_center_raw = ((landmark_pts_raw[3][0] + landmark_pts_raw[4][0])/2.0,
+                        (landmark_pts_raw[3][1] + landmark_pts_raw[4][1])/2.0)
+    mouth_half_size = int(max(x2-x1, y2-y1) / 64 * mouth_target_size[0] / 2)
+    mouth_center_rounded = (int(mouth_center_raw[0]), int(mouth_center_raw[1]))
+    gray_mouth_raw = cv2.resize(gray_image[(mouth_center_rounded[1]-mouth_half_size): (mouth_center_rounded[1]+mouth_half_size),
+                                           (mouth_center_rounded[0]-mouth_half_size): (mouth_center_rounded[0]+mouth_half_size)],
+                                (mouth_target_size))
+    gray_mouth_one = ((gray_mouth_raw / 255.0) - 0.5) * 2
+    gray_mouth_one = np.expand_dims(gray_mouth_one, -1)
+    gray_mouth_one = np.expand_dims(gray_mouth_one, 0)
+    start = time.time()
+    mouth_prediction = mouth_openness_inference.run(gray_mouth_one)
+    print("forward pass of mouth deep nets took {} ms".format((time.time() - start)*1e3))
 
-    if True:
-        gray_face = cv2.resize(gray_face, (emotion_target_size))
-        gray_mouth = gray_face.copy()
-        gray_mouth.fill(127.5)
+    left_right = np.array([mouth_prediction[0][0][0] - mouth_prediction[0][0][2], \
+                           mouth_prediction[0][0][1] - mouth_prediction[0][0][3]])
+    top_down = np.array([mouth_prediction[0][0][4] - mouth_prediction[0][0][6], \
+                         mouth_prediction[0][0][5] - mouth_prediction[0][0][7]])
+    openness = np.array([mouth_prediction[0][0][8] - mouth_prediction[0][0][10], \
+                         mouth_prediction[0][0][9] - mouth_prediction[0][0][11]])
+    left_right_length = np.linalg.norm(left_right)
+    top_down_length = np.linalg.norm(top_down)
+    openness_length = np.linalg.norm(openness)
 
-        mouth_left_corner = ((landmark_pts[3]*w+x - x1) / (x2 - x1) * emotion_target_size[1],
-                             (landmark_pts[8]*h+y - y1) / (y2 - y1) * emotion_target_size[0])
-        mouth_right_corner = ((landmark_pts[4]*w+x - x1) / (x2 - x1) * emotion_target_size[1],
-                              (landmark_pts[9]*h+y - y1) / (y2 - y1) * emotion_target_size[0])
-        center_x = int((mouth_left_corner[0] + mouth_right_corner[0])/2)
-        center_y = int((mouth_left_corner[1] + mouth_right_corner[1])/2)
+    for i in range(6):
+        cv2.circle(gray_mouth_raw, (int(mouth_prediction[0][0][i*2]), int(mouth_prediction[0][0][i*2+1])),
+                   1, (255), -1)
+    cv2.imshow('mouth input 0 {}'.format(mouth_target_size), gray_mouth_raw)
 
-        max_val = 0
-        min_val = 255
+    gray_face = cv2.resize(gray_face, (emotion_target_size))
+    gray_mouth = gray_face.copy()
+    gray_mouth.fill(127.5)
 
-        for i in range(max(0, center_x-16), min(center_x+16, emotion_target_size[0]-1)):
-            for j in range(max(center_y-8, 0), min(center_y+8, emotion_target_size[1]-1)):
-                gray_mouth[j - max(center_y-8, 0) + 40][i-max(0, center_x-16)+16] = gray_face[j][i]
+    mouth_left_corner = ((landmark_pts[3]*w+x - x1) / (x2 - x1) * emotion_target_size[1],
+                         (landmark_pts[8]*h+y - y1) / (y2 - y1) * emotion_target_size[0])
+    mouth_right_corner = ((landmark_pts[4]*w+x - x1) / (x2 - x1) * emotion_target_size[1],
+                          (landmark_pts[9]*h+y - y1) / (y2 - y1) * emotion_target_size[0])
+    center_x = int((mouth_left_corner[0] + mouth_right_corner[0])/2)
+    center_y = int((mouth_left_corner[1] + mouth_right_corner[1])/2)
+
+    max_val = 0
+    min_val = 255
+
+    for i in range(max(0, center_x-16), min(center_x+16, emotion_target_size[0]-1)):
+        for j in range(max(center_y-8, 0), min(center_y+8, emotion_target_size[1]-1)):
+            gray_mouth[j - max(center_y-8, 0) + 40][i-max(0, center_x-16)+16] = gray_face[j][i]
 
 
-        cv2.imshow('mouth input', gray_mouth)
-        # gray_mouth = clahe.apply(gray_mouth)
+    cv2.imshow('mouth input', gray_mouth)
+    # gray_mouth = clahe.apply(gray_mouth)
 
     gray_face = ((gray_face / 255.0) - 0.5) * 2
     gray_face = np.expand_dims(gray_face, -1)
@@ -146,17 +175,21 @@ while True:
     intermediate_val = intermediate_prediction[1].reshape(16, 128).reshape(2048)
     mouth_openness_left_right = max(0, np.dot(intermediate_val, mouth_coef_left_right) + mouth_intercept_left_right)
     mouth_openness_top_down = max(0, np.dot(intermediate_val, mouth_coef_top_down) + mouth_intercept_top_down)
-    mouth_openness_absolute = max(0, np.dot(intermediate_val, mouth_coef_absolute) + mouth_intercept_absolute)
-    print('mouth ab:', mouth_openness_absolute)
     print('mouth lr:', mouth_openness_left_right)
     print('mouth td:', mouth_openness_top_down)
+    print('mouth openness length:', openness_length)
+    print('mouth left right length:', left_right_length)
+    print('mouth top down length:', top_down_length)
 
-    cv2.putText(rgb_image, 'mouth absolute', (10, 8 * 20 + 20), cv2.FONT_HERSHEY_PLAIN, 0.5, (0, 255, 0), 1);
-    cv2.rectangle(rgb_image, (130, 8 * 20 + 10), (130 + int(mouth_openness_absolute * 100), (8 + 1) * 20 + 4), (255, 0, 0), -1)
-    cv2.putText(rgb_image, 'mouth left right', (10, 9 * 20 + 20), cv2.FONT_HERSHEY_PLAIN, 0.5, (0, 255, 0), 1);
+
+    cv2.putText(rgb_image, 'mouth lr SVR', (10, 9 * 20 + 20), cv2.FONT_HERSHEY_PLAIN, 0.5, (0, 255, 0), 1);
     cv2.rectangle(rgb_image, (130, 9 * 20 + 10), (130 + int(mouth_openness_left_right * 100), (9 + 1) * 20 + 4), (255, 0, 0), -1)
-    cv2.putText(rgb_image, 'mouth top down', (10, 10 * 20 + 20), cv2.FONT_HERSHEY_PLAIN, 0.5, (0, 255, 0), 1);
+    cv2.putText(rgb_image, 'mouth td SVR', (10, 10 * 20 + 20), cv2.FONT_HERSHEY_PLAIN, 0.5, (0, 255, 0), 1);
     cv2.rectangle(rgb_image, (130, 10 * 20 + 10), (130 + int(mouth_openness_top_down * 100), (10 + 1) * 20 + 4), (255, 0, 0), -1)
+    cv2.putText(rgb_image, 'mouth lr DNN', (10, 11 * 20 + 20), cv2.FONT_HERSHEY_PLAIN, 0.5, (0, 255, 0), 1);
+    cv2.rectangle(rgb_image, (130, 11 * 20 + 10), (130 + int(openness_length / left_right_length * 100), (11 + 1) * 20 + 4), (255, 0, 0), -1)
+    cv2.putText(rgb_image, 'mouth td DNN', (10, 12 * 20 + 20), cv2.FONT_HERSHEY_PLAIN, 0.5, (0, 255, 0), 1);
+    cv2.rectangle(rgb_image, (130, 12 * 20 + 10), (130 + int(openness_length / top_down_length * 100), (12 + 1) * 20 + 4), (255, 0, 0), -1)
 
     bgr_image = cv2.cvtColor(rgb_image, cv2.COLOR_RGB2BGR)
     cv2.imshow('window_frame', bgr_image)
